fc6c239bf2e513d3e8c93ac91ad60148f17a13f6
Nitri.dev <nitring24@gmail.com>
2018-10-26 15:42:01 +0200

    Added opening file function

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
 
 # your code goes between window and window.mainloop
 window = Tk()
@@ -10,11 +12,19 @@ window.configure(background = 'gray1')
 def save(event):
 	with open('text.txt', 'w') as file:
 		file.write(text1.get("1.0",'end-1c'))
+		
+# function for opening file
+def openfile(event):
+    file = filedialog.askopenfilename()
+    f = open(file)
+    text1.insert(1.0, f.read())
 
 # text area to write
 text1 = Text(window, bg = 'gray1', fg = 'white', insertbackground = 'white')
-text1.bind('<Return>', save) # save file when press enter
-text1.bind('<Escape>', lambda e: window.destroy()) # close window when press escape
+text1.focus_force() # give focus to text area
+text1.bind('<Control-s>', save)
+text1.bind('<Control-o>', openfile)
+text1.bind('<Control-q>', lambda e: window.destroy())
 text1.grid(row = 2, column = 0, columnspan = 50, padx = 5, pady = 5)
 
 window.mainloop()
